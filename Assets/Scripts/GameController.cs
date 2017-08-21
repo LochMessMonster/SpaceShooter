3f8cd9b531412d827848124ff7630081b92f1c3c
LochMessMonster <niveshmodi@gmail.com>
2017-08-21 22:50:13 +0100

    File Add

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+
+	private int score;
+	private bool gameOver;
+	private bool restart;
+
+	void Start () {
+		score = 0;
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+
+		UpdateScores ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update () {
+		if (gameOver) {
+			if (Input.GetKeyDown (KeyCode.R)) {
+				SceneManager.LoadScene(SceneManager.GetActiveScene ().name);
+			}
+		}
+	}
+
+	//to make wait work, it must be made into a coroutine
+	IEnumerator SpawnWaves () {
+		yield return new WaitForSeconds (startWait);
+		while (!gameOver) {
+			for (int i = 0; i < hazardCount; i++) {
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+		}
+
+	}
+
+	public void AddScore (int newScoreValue) {
+		score += newScoreValue;
+		UpdateScores ();
+	}
+
+	void UpdateScores () {
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver () {
+		gameOverText.text = "Game Over!";
+		gameOver = true;
+		restartText.text = "Press 'R' for restart.";
+		restart = true;
+	}
+}
